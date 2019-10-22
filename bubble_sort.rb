612d26a2bed1baa3b2aef638db0be86cb73ea8ef
MaryAnn Chukwuka <annychuks07@gmail.com>
2019-10-22 22:34:20 +0100

    adjust linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
-def bubble_sort (arr)
+
+def bubble_sort(arr)
   length = arr.length
-  for i in 0..length-1 do
+  for i in 0..length - 1 do
     for j in 0..length-i-2 do
       if arr[j] > arr[j + 1]
-        carry = arrToSort[j+ 1]
+        carry = arrToSort[j + 1]
         arr[j + 1] = arr[j]
         arr[j] = carry
       end
@@ -13,23 +14,24 @@ def bubble_sort (arr)
   arr
 end
 
-def bubble_sort_by arr
-  for i in 0..arr.length-2 do
-    for j in 1..arr.length - (i+1) do
+def bubble_sort_by(arr)
+  for i in 0..arr.length - 2 do
+    for j in 1..arr.length - (i + 1) do
       if block_given?
-        arr[j-1],arr[j] = arr[j],arr[j-1] if yield(arr[j-1], arr[j]) > 0
+        arr[j - 1],arr[j] = arr[j],arr[j - 1] if yield(arr[j - 1], arr[j]).positive?
       else
-        arr[j-1],arr[j] = arr[j],arr[j-1] if arr[j-1] > arr[j]
+        arr[j - 1],arr[j] = arr[j],arr[j - 1] if arr[j - 1] > arr[j]
       end
     end
   end
   p arr
 end
 
-bubble_sort_by(['hi','hello','hey']) do |left, right|
+word_list = %w['hi','hello','hey']
+bubble_sort_by(word_list) do |left, right|
   left.length - right.length
 end
 
-p ['hi','hello','hey'].sort do |left, right|
+p word_list.sort do |left, right|
   left.length - right.length
 end
