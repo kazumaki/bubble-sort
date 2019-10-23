91cc266fc7de9f2bfa7c197fa09bca3226dafa57
Kazumaki <vcampos.pitangui@gmail.com>
2019-10-23 12:30:53 -0300

    Replace for by each

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,10 +2,10 @@
 
 def bubble_sort(arr)
   length = arr.length
-  for i in 0..length - 1 do
-    for j in 0..length-i-2 do
+  (0..length - 1).each do |i|
+    (0..length - i - 2).each do |j|
       if arr[j] > arr[j + 1]
-        carry = arrToSort[j + 1]
+        carry = arr[j + 1]
         arr[j + 1] = arr[j]
         arr[j] = carry
       end
@@ -15,8 +15,8 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
-  for i in 0..arr.length - 2 do
-    for j in 1..arr.length - (i + 1) do
+  (0..arr.length - 2).each do |i|
+    (1..arr.length - (i + 1)).each do |j|
       if block_given?
         if yield(arr[j - 1], arr[j]).positive?
           arr[j - 1], arr[j] = arr[j], arr[j - 1]
@@ -31,7 +31,7 @@ def bubble_sort_by(arr)
   p arr
 end
 
-word_list = %w[hi, hello, hey]
+word_list = %w[hi hello hey]
 bubble_sort_by(word_list) do |left, right|
   left.length - right.length
 end
